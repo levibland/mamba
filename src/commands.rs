408d20fd4ceeaeb4acb92810499a957a13f59bbf
levibland <levi_bland22@protonmail.com>
2022-07-05 14:24:26 +1200

    feat: add working interpreter

---
+++ src/commands.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+pub enum Command {
+    FileRead(String),
+    RunInlineCode(String),
+    Noop,
+}
+
+pub fn read_command() -> Command {
+    let matches = clap_app!(mamba =>
+        (version: "0.3.0\n")
+        (author: "Levi Bland <levi.bland@icloud.com>\n")
+        (about: "The Mamba Programming Language")
+        (@setting ArgRequiredElseHelp)
+        (@arg src: -s --src +takes_value "Path of the source file")
+        (@arg run: -r --run +takes_value "Code you want to run inline")
+    ).get_matches();
+
+    let src_path = matches.value_of("src").map(|s| s.to_string());
+    let run_string = matches.value_of("run").map(|s| s.to_string());
+    match (src_path, run_string) {
+        (Some(s), _) => Command::FileRead(s),
+        (_, Some(s)) => Command::RunInlineCode(s),
+        _ => Command::Noop,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[macro_use] extern crate clap;
+
+use mamba::evaluator::*;
+use mamba::lexer::*;
+use mamba::parser::*;
+use std::fs::File;
+use std::io::prelude::*;
+
+mod commands;
+use crate::commands::*;
+
+fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let code_string = match commands::read_command() {
+        Command::FileRead(file_path) => read_file(file_path).ok(),
+        Command::RunInlineCode(code) => Some(code),
+        Command::Noop => None,
+    };
+
+    if let Some(code_string) = code_string {
+        let lexer = Lexer::new(code_string);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program();
+        parser.check_parser_errors();
+
+        let mut evaluator = Evaluator::new();
+        let eval = evaluator.eval_program(program);
+        println!("{}", eval);
+    }
 }
